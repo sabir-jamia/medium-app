3dfe9ce1cc2d57db5fba51c8229bb0eda292de63
Mohammad Sabir <sabir.jmi11@gmail.com>
2020-06-12 11:20:32 +0530

    return only jsx

--- functions/utils/loader.js
+++ functions/utils/loader.js
@@ -3,17 +3,17 @@ const babel = require('@babel/core');
 const { renderToStaticMarkup } = require('react-dom/server');
 const mdx = require('@mdx-js/mdx');
 const { MDXProvider, mdx: createElement } = require('@mdx-js/react');
-require('@babel/preset-react');
+const jsxTransformer = require('@babel/plugin-transform-react-jsx');
 
 const transform = code =>
    babel.transformSync(code, {
-      presets: ['@babel/preset-react'],
+      plugins: [jsxTransformer],
    }).code;
 
 module.exports = async mdxCode => {
    const jsx = await mdx(mdxCode, { skipExport: true });
-   return jsx;
    const code = transform(jsx);
+
    const scope = { mdx: createElement };
 
    const fn = new Function(
